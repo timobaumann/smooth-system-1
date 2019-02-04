e6fad03b0d23ede59b53260ede37255e785418d5
Timo Baumann <baumann@informatik.uni-hamburg.de>
2019-02-04 21:54:44 +0100

    robotsim with visualization and status updates

--- robotsim.py
+++ robotsim.py
@@ -6,9 +6,19 @@ from random import randint
 from time import sleep
 
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard " + data.data)
-    sleep(randint(5,10))
-    pub.publish('approach_finished')
+    rospy.loginfo(rospy.get_caller_id() + ": I heard " + data.data)
+    distance = randint(5,10)
+    mypub('approach_initiated')
+    for i in range(distance):
+        sleep(1)
+        mypub('approach_ETA({})'.format(distance-i))
+        rospy.loginfo(rospy.get_caller_id() + ": distance travelled {}{}".format("+"*i, "-"*(distance-i-1)))
+    rospy.loginfo(rospy.get_caller_id() + ": simulated approach finished")
+    mypub('approach_finished')
+
+def mypub(message):
+    rospy.loginfo(rospy.get_caller_id() + ": published message '" + message + "'.")
+    pub.publish(message)
 
 def run():
     rospy.init_node('robot')
